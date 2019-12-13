56b47af2fbb2a49c9b0183688ad0a80f32a43776
José Luis Recio <reciojoselu@gmail.com>
2019-12-13 22:22:31 +0100

    Add chip position

--- client/src/allTeams/allTeams.component.jsx
+++ client/src/allTeams/allTeams.component.jsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Container from "@material-ui/core/Container";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Tabs from "@material-ui/core/Tabs";
@@ -13,7 +12,6 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import EuroIcon from '@material-ui/icons/Euro';
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
